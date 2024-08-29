de6d61db75b992e01e15ec70df43459578b46545
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-08-29 19:21:48 +0300

    profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,30 @@
 export default function Profile({name, tag, location, image, stats}) {
     return (
         <>
+         <div>
+    <img
+      src={image}
+      alt={name}
+    />
+    <p>{name}</p>
+    <p>@{tag}</p>
+    <p>{location}</p>
+  </div>
+
+  <ul>
+    <li>
+      <span>{stats.followers(key)}</span>
+      <span>{stats.followers[key]}</span>
+    </li>
+    <li>
+      <span>Views</span>
+      <span>2000</span>
+    </li>
+    <li>
+      <span>Likes</span>
+      <span>3000</span>
+    </li>
+  </ul>
         </>
     )
 }
